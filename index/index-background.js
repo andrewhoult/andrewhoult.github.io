fd0c531f0daab642fa5123e9481bfd308851dc78
andrewh <mrrobottrax@gmail.com>
2025-09-17 16:47:31 -0400

    Get device

---
+++ index/index-background.js
@@ -0,0 +1,46 @@
+let g_BackgroundRenderer;
+
+class BackgroundRenderer {
+	m_Canvas;
+	m_Device;
+	m_Context;
+
+	async init(canvas) {
+		console.log("Renderer init");
+		this.m_Canvas = canvas;
+
+		if (!this.m_Canvas) {
+			throw Error("No canvas.");
+		}
+
+		if (!navigator.gpu) {
+			throw Error("WebGPU not supported.");
+		}
+
+		const adapter = await navigator.gpu.requestAdapter();
+		if (!adapter) {
+			throw Error("Couldn't request WebGPU adapter.");
+		}
+
+		this.m_Device = await adapter.requestDevice();
+		if (!this.m_Device) {
+			throw Error("Couldn't request WebGPU device.");
+		}
+
+		this.m_Context = this.m_Canvas.getContext("webgpu");
+		if (!this.m_Context) {
+			throw Error("Couldn't get webgpu context.");
+		}
+		this.m_Context.configure({
+			device: this.m_Device,
+			format: navigator.gpu.getPreferredCanvasFormat(),
+			alphaMode: "premultiplied",
+		});
+	}
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	let canvas = document.getElementById("funny-background-canvas");
+	g_BackgroundRenderer = new BackgroundRenderer();
+	g_BackgroundRenderer.init(canvas);
+});
